620e9d4ee8ba1a29c550a58d4a7d0f180c804b36
Merhawi <merhawiainealem@gmail.com>
2024-12-07 16:47:24 +0100

    new deployment

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,10 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  build: {
-    chunkSizeWarningLimit: 2000, // Set the warning limit to 1000 kB
-    rollupOptions: {
-      input: './index.html', // Specify your entry point
-    },
-    outDir: 'dist',
-  },
+  plugins: [react()],
+  
 });
