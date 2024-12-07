1fced3019fb24bbb5cf8299992bafaab8e251ab9
Merhawi <merhawiainealem@gmail.com>
2024-12-07 01:31:22 +0100

    fix configaration problem

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { ghPages } from 'vite-plugin-gh-pages';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), ghPages()],
+//   build: {
+//     chunkSizeWarningLimit: 2000, // Set the warning limit to 1000 kB
+//   },
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
@@ -7,5 +19,8 @@ export default defineConfig({
   plugins: [react(), ghPages()],
   build: {
     chunkSizeWarningLimit: 2000, // Set the warning limit to 1000 kB
+    rollupOptions: {
+      input: './index.html', // Specify your entry point
+    },
   },
-})
+});
